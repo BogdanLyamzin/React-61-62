2b6c5fd0b9a67e50fa91930d4c8b0852423ed70a
unknown <bogdan.lyamzin.d@gmail.com>
2023-02-03 21:45:08 +0200

    fix

---
+++ lesson-9-useEffect-useRef--memo-useCallback-useMemo/useEffect-example/src/modules/MyBooks/MyBooks.jsx
@@ -0,0 +1,178 @@
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+
+import MyBooksForm from "./MyBooksForm/MyBooksForm";
+import MyBookList from "./MyBookList/MyBookList";
+import MyBooksFilter from "./MyBooksFilter/MyBooksFilter";
+
+import styles from "./my-books.module.scss";
+
+const MyBooks = () => {
+    const [books, setBooks] = useState(()=> {
+        const books = JSON.parse(localStorage.getItem("my-books"));
+        return books ? books : [];
+    });
+    const [filter, setFilter] = useState("");
+
+    useEffect(()=> {
+        localStorage.setItem("my-books", JSON.stringify(books));
+    }, [books]);
+
+    const isDublicate = (title, author) => {
+        const normalizedTitle = title.toLowerCase();
+        const normalizedAuthor = author.toLowerCase();
+        const result = books.find(({ title, author }) => {
+            return (title.toLowerCase() === normalizedTitle && author.toLowerCase() === normalizedAuthor)
+        })
+
+        return Boolean(result)
+    }
+
+    const addBook = ({ title, author }) => {
+        if (isDublicate(title, author)) {
+            alert(`${title}. Author: ${author} is already ixist`);
+            return false;
+        }
+
+        setBooks(prevBooks => {
+            const newBook = {
+                id: nanoid(),
+                title,
+                author,
+            }
+
+            return [newBook, ...prevBooks]
+        })
+        return true;
+    }
+
+    const removeBook = (id) => {
+        setBooks(prevBooks => prevBooks.filter(item => item.id !== id))
+    }
+
+    const handleFilter = ({ target }) => setFilter(target.value);
+
+    const getFilteredBooks = () => {
+        if (!filter) {
+            return books;
+        }
+
+        const normalizedFilter = filter.toLowerCase();
+        const result = books.filter(({ title, author }) => {
+            return (title.toLowerCase().includes(normalizedFilter) || author.toLowerCase().includes(normalizedFilter))
+        })
+
+        return result;
+    }
+
+    const filteredBooks = getFilteredBooks();
+    const isBooks = Boolean(filteredBooks.length);
+
+    return (
+        <div>
+            <h3>My Books</h3>
+            <div className={styles.wrapper}>
+                <div className={styles.block}>
+                    <h4>Add book</h4>
+                    <MyBooksForm onSubmit={addBook} />
+                </div>
+                <div className={styles.block}>
+                    <MyBooksFilter handleChange={handleFilter} />
+                    {isBooks && <MyBookList removeBook={removeBook} items={filteredBooks} />}
+                    {!isBooks && <p>No books in list</p>}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MyBooks;
+
+/*
+class MyBooks extends Component {
+
+    state = {
+        items: [...items],
+        filter: "",
+    }
+
+    removeBook = (id) => {
+        this.setState(({items}) => {
+            const newBooks = items.filter(item => item.id !== id);
+            return {items: newBooks}
+        })
+    }
+
+    addBook = ({title, author}) => {
+        if(this.isDublicate(title, author)) {
+            alert(`${title}. Author: ${author} is already ixist`); // Notify.Alert(`${title}. Author: ${author} is already ixist`)
+            return false;
+        }
+
+        this.setState(prevState => {
+            const {items} = prevState;
+
+            const newBook = {
+                id: nanoid(),
+                title,
+                author,
+            }
+
+            return {items: [newBook, ...items]}
+        })
+        return true;
+    }
+
+    handleFilter = ({target})=> {
+        this.setState({filter: target.value})
+    }
+
+    isDublicate(title, author) {
+        const normalizedTitle = title.toLowerCase();
+        const normalizedAuthor = author.toLowerCase();
+        const {items} = this.state;
+        const result = items.find(({title, author}) => {
+            return (title.toLowerCase() === normalizedTitle && author.toLowerCase() === normalizedAuthor)
+        })
+
+        return Boolean(result)
+    }
+
+    getFilteredBooks() {
+        const {filter, items} = this.state;
+        if(!filter) {
+            return items;
+        }
+        
+        const normalizedFilter = filter.toLowerCase()
+        const result = items.filter(({title, author})=> {
+            return (title.toLowerCase().includes(normalizedFilter) ||  author.toLowerCase().includes(normalizedFilter))
+        })
+
+        return result;
+    }
+
+    render() {
+        const {addBook, removeBook, handleFilter} = this;
+        const items = this.getFilteredBooks();
+        const isBooks = Boolean(items.length);
+  
+        return (
+            <div>
+                <h3>My Books</h3>
+                <div className={styles.wrapper}>
+                    <div className={styles.block}>
+                        <h4>Add book</h4>
+                        <MyBooksForm onSubmit={addBook} />
+                    </div>
+                    <div className={styles.block}>
+                        <MyBooksFilter handleChange={handleFilter} />
+                        {isBooks && <MyBookList removeBook={removeBook} items={items} />}
+                        {!isBooks && <p>No books in list</p>}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+*/
---
+++ lesson-9-useEffect-useRef--memo-useCallback-useMemo/useEffect-example/src/modules/PostsSearch/PostsSearchForm/PostsSearchForm.jsx
@@ -0,0 +1,88 @@
+import { memo } from "react";
+import PropTypes from "prop-types";
+
+import useForm from "../../../shared/hooks/useForm";
+
+import styles from "./posts-search-form.module.scss";
+
+import initialState from "./initialState";
+
+const PostsSearchForm = ({onSubmit}) => {
+    const {state, handleChange, handleSubmit} = useForm({initialState, onSubmit});
+    console.log("Render form")
+    /*
+    const [state, setState] = useState({...initialState})
+
+    const handleChange = ({target}) => {
+        const {name, value} = target;
+        setState(prevState => {
+            return {...prevState, [name]: value}
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        onSubmit(({...state}));
+        setState({...initialState});
+    }
+    */
+
+    const {search} = state;
+
+    return (
+        <form className={styles.form} onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="">Search posts</label>
+                <input value={search} onChange={handleChange} name="search" placeholder="Search posts" required />
+            </div>
+        </form>
+    )
+}
+
+export default memo(PostsSearchForm);
+
+PostsSearchForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+}
+
+/*
+class PostsSearchForm extends Component {
+
+    state = {
+        search: "",
+    }
+
+    handleChange = ({ target }) => {
+        const { name, value } = target;
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const {onSubmit} = this.props;
+        onSubmit({...this.state});
+        this.reset()
+    }
+
+    reset(){
+        this.setState({
+            search: "",
+        })
+    }
+
+    render() {
+        const {search} = this.state;
+        const {handleChange, handleSubmit} = this;
+
+        return (
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="">Search posts</label>
+                    <input value={search} onChange={handleChange} name="search" placeholder="Search posts" required />
+                </div>
+            </form>
+        )
+    }
+}
+
+*/
